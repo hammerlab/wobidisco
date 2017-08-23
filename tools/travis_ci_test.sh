ad525f78a0f253184bbd402f82f858c85dfcba2c
Sebastien Mondet <seb@mondet.org>
2017-08-23 13:50:49 -0400

    Add a Travis CI setup

---
+++ tools/travis_ci_test.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+
+
+travis_install_on_linux () {
+    # Install OCaml and OPAM PPAs
+    export ppa=avsm/ocaml42+opam12
+
+    echo "yes" | sudo add-apt-repository ppa:$ppa
+    sudo apt-get update -qq
+
+    export opam_init_options="--comp=$OCAML_VERSION"
+    sudo apt-get install -qq  opam time git
+}
+
+travis_install_on_osx () {
+    curl -OL "http://xquartz.macosforge.org/downloads/SL/XQuartz-2.7.6.dmg"
+    sudo hdiutil attach XQuartz-2.7.6.dmg
+    sudo installer -verbose -pkg /Volumes/XQuartz-2.7.6/XQuartz.pkg -target /
+
+    brew update
+    brew install opam
+    export opam_init_options="--comp=$OCAML_VERSION"
+}
+
+
+case $TRAVIS_OS_NAME in
+  osx) travis_install_on_osx ;;
+  linux) travis_install_on_linux ;;
+  *) echo "Unknown $TRAVIS_OS_NAME"; exit 1
+esac
+
+# configure and view settings
+export OPAMYES=1
+echo "ocaml -version"
+ocaml -version
+echo "opam --version"
+opam --version
+echo "git --version"
+git --version
+
+# install OCaml packages
+opam init $opam_init_options
+eval `opam config env`
+
+opam update
+
+opam pin add -n ketrew https://github.com/hammerlab/ketrew.git
+opam pin add -n biokepi https://github.com/hammerlab/biokepi.git
+opam pin add -n epidisco https://github.com/hammerlab/epidisco.git
+
+opam install --yes biokepi ketrew epidisco
+
+echo 'ocamlfind list | grep lwt'
+ocamlfind list | grep lwt
+echo 'ocamlfind list | grep cohttp'
+ocamlfind list | grep cohttp
+
+echo "Setting Warn-Error for the Travis test"
+export OCAMLPARAM="warn-error=A,_"
+
+# We try the example
+cat > my_cluster.ml <<EOCAML
+module My_cluster = struct
+  let max_processors = 42
+  let work_dir = "/work/dir/"
+  let results_dir = "/results/dir/"
+  let datasets_home = "/datasets/"
+  let machine =
+    Biokepi.Setup.Build_machine.create "ssh://example.com/tmp/KT/"
+end
+EOCAML
+ls -la my_cluster.ml
+cat my_cluster.ml
+
